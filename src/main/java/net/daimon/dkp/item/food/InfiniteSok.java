2081c3713e0002dc6fec5c09169d507d9a12285f
daimon <dsamojlov04@gmail.com>
2026-01-05 03:20:59 +0700

    Added infinite "property" to InfiniteSok

--- src/main/java/net/daimon/dkp/item/food/InfiniteSok.java
+++ src/main/java/net/daimon/dkp/item/food/InfiniteSok.java
@@ -1,14 +1,18 @@
 package net.daimon.dkp.item.food;
 
 import net.minecraft.component.type.FoodComponent;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.UseAction;
+import net.minecraft.world.World;
 
 public class InfiniteSok extends Item {
 
     public InfiniteSok() {
         super(new Item.Settings()
+                .maxCount(1)
                 .food(
                         new FoodComponent.Builder()
                                 .alwaysEdible()
@@ -23,4 +27,15 @@ public class InfiniteSok extends Item {
         return UseAction.DRINK;
     }
 
+    @Override
+    public ItemStack finishUsing(ItemStack stack, World world, LivingEntity user) {
+        ItemStack result = super.finishUsing(stack.copy(), world, user);
+
+        if (user instanceof PlayerEntity player && !player.isCreative()) {
+            // если предмет должен быть бесконечным — возвращаем его обратно
+            return stack;
+        }
+
+        return result;
+    }
 }
